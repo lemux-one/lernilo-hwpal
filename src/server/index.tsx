590cf14a22e5c3be830ab864d86d6a52574d4a57
Luis Lemus <lemux.one@gmail.com>
2024-10-14 19:19:53 -0400

    used Layout to wrap Home page for path "/"

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,13 +1,18 @@
-import { start, type Route } from "./http";
-import { Home } from "./pages/Home";
 import { h } from "../common/h";
+import { start, type Route } from "./http";
+import { Layout } from "./pages/Layout";
+import { Home } from "./pages/Home";
 
 const routes: Route[] = [
   {
     acceptsRequest: (req) => req.url === "/",
     handler: ({ res }) => {
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(<Home />);
+      res.end(
+        <Layout>
+          <Home />
+        </Layout>
+      );
     },
   },
 ];
