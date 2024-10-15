5f7f4628103d9128803d69a22019d7bfc4784c50
Luis Lemus <lemux.one@gmail.com>
2024-10-14 22:32:00 -0400

    fixed isBrowser helper utility

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,20 +1,22 @@
 function isEmpty(data: any) {
-    return data === null || data === undefined;
+  return data === null || data === undefined;
 }
 
 function isObject(data: any) {
-    return data && typeof data === "object";
+  return data && typeof data === "object";
 }
 
 function isBlank(data: any) {
-    return isEmpty(data) ||
-        (Array.isArray(data) && data.length === 0) ||
-        (isObject(data) && Object.keys(data).length === 0) ||
-        (typeof data === "string" && data.trim().length === 0);
+  return (
+    isEmpty(data) ||
+    (Array.isArray(data) && data.length === 0) ||
+    (isObject(data) && Object.keys(data).length === 0) ||
+    (typeof data === "string" && data.trim().length === 0)
+  );
 }
 
 function isBrowser() {
-    return typeof window === undefined;
+  return typeof window !== "undefined";
 }
 
 export { isBlank, isBrowser, isEmpty, isObject };
